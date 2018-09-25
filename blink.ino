ea64ed28fac0a93e850737653a0034056e1ee136
Iñaki <iortizaldamiz@gmail.com>
2018-09-25 16:43:33 +0200

    zuzenduta

--- blink.ino
+++ blink.ino
@@ -1,10 +1,10 @@
-void setups() {
+void setup() {
   pinMode(LED_BUILTIN, INPUT);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   sleep(1000);
-  digitalRead(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   sleep(2000);
 }
